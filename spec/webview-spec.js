146974bfd195f18bdba11cacc1d607edd0650703
Shelley Vohr <shelley.vohr@gmail.com>
2018-05-21 21:45:49 -0700

    testing fix

--- spec/webview-spec.js
+++ spec/webview-spec.js
@@ -1178,7 +1178,7 @@ describe('<webview> tag', function () {
   // TODO(alexeykuzmin): Enable the test.
   // The app crashes if this test and "devtools-opened event" tests
   // are run at the same time.
-  xit('loads devtools extensions registered on the parent window', (done) => {
+  it('loads devtools extensions registered on the parent window', (done) => {
     w = new BrowserWindow({ show: false })
     BrowserWindow.removeDevToolsExtension('foo')
 
